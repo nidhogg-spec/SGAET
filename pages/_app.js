ca477842fef437858abb4879393becd16d6bf344
carlos schiavonne hurtado <carlosdragon168@gmail.com>
2020-10-30 14:25:49 -0500

    Añadir Archivos de configuracion

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
 
+//this import is using the next.config,js how we see we aren't specification the exact path
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    /*Here is where the indexjs is rendering is something like the index in react you can also
+    add the footer under the component and will work like the footer don't delete the <> </> cause throws and error
+    */
+   
+    <>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
