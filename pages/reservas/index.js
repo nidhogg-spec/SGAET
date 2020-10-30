972bb54225ec500f319f1d280bee82a3bafd7b74
Sisterno <g_onza_linn@hotmail.com>
2020-10-30 16:08:32 -0500

    Primera

---
+++ pages/reservas/index.js
@@ -0,0 +1,14 @@
+import Hero from '../components/Hero/Hero'
+import Destination from '../components/FeaturedDestination/FeaturedDestination'
+import InfoBox from '../components/InfoBox/InfoBox'
+import LandingListTours from '../components/LandingListTours/LandingListTours'
+import MostVisitedDestination from '../components/MostVisitedDestination/MostVisitedDestination'
+import LandingCategorie from '../components/LandingCategorie/LandingCategorie'
+// import React,{useState,useEffect} from 'react'
+
+export default function Home() {
+  return (
+    <div>
+    </div>
+  )
+}
