8f9b5e1695f8b0262d7c4d25ceef32b69d2a04f3
Sisterno <g_onza_linn@hotmail.com>
2020-11-02 13:51:00 -0500

    Animacion mamalona

---
+++ components/navLateral/components/modulo.js
@@ -0,0 +1,79 @@
+import styles from "./modulo.module.css";
+import Link from "next/link";
+
+function AnimationSubModulos(e){
+    let x = HTMLElement;
+    x  =  e.target.parentNode.children[1];
+    if(x.style.visibility=="hidden"){
+        x.style.height="auto"
+        x.style.padding="0.5em 0 0.5em 50px"
+        x.style.visibility="visible"
+    }else{
+        x.style.height="0"
+        x.style.padding="0 0 0 0"
+        x.style.visibility="hidden"
+    }
+    
+
+    
+}
+
+const SubModulos = ({dataSubModulos=[]}) => {
+    if (dataSubModulos.tipo=="link") {
+        return(
+        <Link href={dataSubModulos.link}>
+            <a>{dataSubModulos.subModuloName}</a>
+        </Link>
+        )
+
+    }else{
+        return ( 
+            <div>
+                
+            </div>
+         );
+    }
+    
+}
+
+const modulos = ({
+  data = {
+    modulo: "TituloModlo",
+    svg:"account_box-black-18dp.svg",
+    subModulos:[
+        {
+            tipo:"link",
+            subModuloName:"",
+            link:"#"
+        },{
+            tipo:"sub",
+            subModuloName:"",
+            subSubModulos:[
+                {subSubModuloName:"",
+                link:"#"}
+            ]
+        },
+    ]
+  }
+}) => {
+  return (
+    <div>
+      <div className={styles.NavLateralModulos} onClick={AnimationSubModulos} >
+        <img src={"/resources/"+data.svg} />
+        {data.modulo}
+        <img src="/resources/keyboard_arrow_down-black-18dp.svg" />
+      </div>
+      <div className={styles.NavLateralSubModulos}>
+        {data.subModulos.map((dataSubModulosInner) => (
+          <SubModulos dataSubModulos={dataSubModulosInner} key={data.subModulos.indexOf(dataSubModulosInner)}/>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+
+
+export default modulos;
+
+ 
