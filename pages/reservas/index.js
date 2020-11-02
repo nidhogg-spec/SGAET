a906de3b527a1d1df932ab581d60ab012914fa4e
Sisterno <g_onza_linn@hotmail.com>
2020-11-01 19:35:12 -0500

    header :)

--- pages/reservas/index.js
+++ pages/reservas/index.js
@@ -1,10 +1,12 @@
 
 // import React,{useState,useEffect} from 'react'
 import NavLateral from '../../components/navLateral/NavLateral'
+import Header from '../../components/header/Header'
 
 export default function Home() {
   return (
     <div>
+      <Header/>
       <NavLateral/>
     </div>
   )
