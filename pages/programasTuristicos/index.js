9c8cd59da71a1558a85c3024b068764c32b34837
carlos schiavonne hurtado <carlosdragon168@gmail.com>
2020-11-07 12:01:16 -0500

    iconos en material table

--- pages/programasTuristicos/index.js
+++ pages/programasTuristicos/index.js
@@ -1,29 +1,39 @@
 // import React,{useState,useEffect} from 'react'
+import { colors } from "@material-ui/core";
+
 import MaterialTable from "material-table";
 
+
+let columns=[
+  { title: "Nomber Programa", field: "name" },
+  { title: "Localizacion", field: "surname" },
+  { title: "Duracion Dias", field: "dayduration", type: "numeric" },
+  { title: "Duracion Noches", field: "nightduration", type: "numeric" },
+  {
+    title: "",
+    field: "icon",
+    type: "icon"
+  },
+]
+let data=[
+  {
+    name: "Mehmet",
+    surname: "Baran",
+    dayduration: 10,
+    nightduration: 15,
+    birthCity: 63,
+    icon: tableEyeIcon
+  },
+]
+
 export default function Home() {
+
   return (
     <div>
       <MaterialTable
-      columns={[
-        { title: "Nomber Programa", field: "name" },
-        { title: "Localizacion", field: "surname" },
-        { title: "Duracion Dias", field: "birthYear", type: "numeric" },
-        { title: "Duracion Noches", field: "birthYear", type: "numeric" },
-        {
-          title: "Doğum Yeri",
-          field: "birthCity",
-          lookup: { 34: "İstanbul", 63: "Şanlıurfa" },
-        },
-      ]}
-      data={[
-        {
-          name: "Mehmet",
-          surname: "Baran",
-          birthYear: 1987,
-          birthCity: 63,
-        },
-      ]}
+      columns={columns}
+      data={data}
+      icons= {tableEyeIcon,tablePenIcon}
       title="Programas Turisticos"
       />
     </div>
