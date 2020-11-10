57358d687117eec9d3d18f4b71e25f9655b91e22
Sisterno <g_onza_linn@hotmail.com>
2020-11-10 16:59:48 -0500

    Todo correcto y yo q me alegro , version npm

--- components/Modal/Modal.js
+++ components/Modal/Modal.js
@@ -2,23 +2,27 @@ import styles from "./Modal.module.css";
 import { MongoClient } from 'mongodb';
 import react from "react";
 
-class Modal extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      TipoModal:"error",
-      data:{},
-      id:"",
-      coleccion:"",
-      accion:"sin accion"
-    };
+export default function Modal (){
+  //hooks 
+  const [TipoModal, setTipoModal] = useState('');
+  const [Data, setData] = useState({});
+  const [Id, setId] = useState('');
+  const [Coleccion, setColeccion] = useState('');
+  const [Accion, setAccion] = useState('');
+
+  
+  function componentDidMount(){
+    if(this.state.accion=="sin accion"){
+  
+    }
   }
-  getData = () =>{
+  
+  const getData = () =>{
     const jwtsecret= process.env.SECRET_KEY;
     const saltRounds=10;
     const url= process.env.MONGODB_URI
     const dbName= process.env.MONGODB_DB
-
+  
     let client = new MongoClient(url,{
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -26,7 +30,7 @@ class Modal extends Component {
     client.connect(e=>{
       let dbo=client.db(dbName);
       let collection = dbo.collection(this.state.coleccion)
-
+  
       collection.findOne({idServicio: this.state.id},(err,result)=>{
         if(err){
             client.close()
@@ -39,34 +43,42 @@ class Modal extends Component {
       })
     })
   }
-  componentDidMount(){
-    if(this.state.accion=="sin accion"){
-      
-    }
-  }
-
-
-
-  render() {
-    return (
-      <div
-        id="myModalXD"
-        className={styles.Modal}
-        onClick={(event) => {
-          let modal = document.getElementById("myModalXD");
-          console.log(modal);
-          if (event.target == modal) {
-            modal.style.display = "none";
-          }
-        }}
-      >
-        <div className={styles.Modal_content}></div>
-      </div>
-    );
-  }
+  return (
+    <div
+      id="myModalXD"
+      className={styles.Modal}
+      onClick={(event) => {
+        let modal = document.getElementById("myModalXD");
+        console.log(modal);
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      }}
+    >
+      <div className={styles.Modal_content}></div>
+    </div>
+  );
 }
 
-export default Modal;
+
+
+// class Modal extends Component {
+  
+  
+//   componentDidMount(){
+//     if(this.state.accion=="sin accion"){
+
+//     }
+//   }
+
+
+
+//   render() {
+    
+//   }
+// }
+
+// export default Modal;
 // export default Modal = () => {
 
 //   return (
